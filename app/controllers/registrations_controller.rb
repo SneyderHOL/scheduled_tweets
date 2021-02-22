3049e7c140fefa6bf57965c81584c017e6d82149
SneyderHOL <1507@holbertonschool.com>
2021-02-21 19:37:28 -0500

    [UPDATE]: add current user funcionality

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,12 +9,13 @@ class RegistrationsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Successfully created account"
     else
-      flash[:alert] = "Something was wrong"
+      # flash[:alert] = "Something was wrong"
       render :new
     end
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
